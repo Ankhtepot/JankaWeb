a675aeebdf9adb6ce345229fbe1056984f32cde2
Zavodny Petr <petr.zavodny@sensearena.com>
2024-06-05 18:08:07 +0200

    implemented EmailJS

--- src/app/pages/contact/contact-form/contact-form.component.ts
+++ src/app/pages/contact/contact-form/contact-form.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import {T} from "../../../services/text.service";
 import {FormGroup} from "@angular/forms";
+import emailjs, {EmailJSResponseStatus} from 'emailjs-com';
+import {environment} from "../../../../environments/environment";
 
 @Component({
   selector: 'app-contact-form',
@@ -9,15 +11,33 @@ import {FormGroup} from "@angular/forms";
 })
 export class ContactFormComponent {
   contact = {
-    meno: '',
+    name: '',
     email: '',
-    zprava: ''
+    message: ''
   };
 
   onSubmit(form: FormGroup) {
     if (form.valid) {
-      console.log('Sending email:', this.contact);
-      // Implement email sending functionality here
+      const body = {
+        to_name: 'Janka',
+        web_name: 'JankaWeb',
+        from_name: this.contact.name,
+        email: this.contact.email,
+        message: this.contact.message
+      };
+
+      emailjs.send(
+        environment.emailServiceId,
+        environment.emailTemplateId,
+        body,
+        environment.emailUserId)
+        .then((response: EmailJSResponseStatus) => {
+          console.log('SUCCESS!', response.status, response.text);
+          alert('Email sent successfully!');
+        }, (error) => {
+          console.log('FAILED...', error);
+          alert('Failed to send email. Please try again later.');
+        });
     }
   }
 
